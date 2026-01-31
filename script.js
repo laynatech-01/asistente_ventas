b7f247fec6716db617511ab44b43aabc49043b46
laynatech-01 <laynatech@gmail.com>
2026-01-31 10:48:07 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Función original para mostrar/ocultar chat
+// Función para mostrar/ocultar chat
 function toggleChat() {
     const chatWindow = document.getElementById('chat-window');
     if (chatWindow.style.display === 'none' || chatWindow.style.display === '') {
@@ -8,7 +8,7 @@ function toggleChat() {
     }
 }
 
-// Nueva función para cargar las URLs de las inmobiliarias
+// Función para cargar las URLs
 function cargarSitio(url) {
     const iframe = document.getElementById('central-frame');
     iframe.src = url;
