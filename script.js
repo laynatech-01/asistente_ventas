70edb489b5d8d70167f38170b1d8147cc9103ae7
laynatech-01 <laynatech@gmail.com>
2026-01-31 10:56:30 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Función para mostrar/ocultar chat
+// Función original para mostrar/ocultar chat
 function toggleChat() {
     const chatWindow = document.getElementById('chat-window');
     if (chatWindow.style.display === 'none' || chatWindow.style.display === '') {
@@ -8,7 +8,7 @@ function toggleChat() {
     }
 }
 
-// Función para cargar las URLs
+// Función para cargar las URLs en el frame central
 function cargarSitio(url) {
     const iframe = document.getElementById('central-frame');
     iframe.src = url;
